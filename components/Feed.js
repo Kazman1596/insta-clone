58f266636149eec4b69afe7d1c4b80237872edac
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2022-09-28 13:40:00 -0700

    populate fake data using mini-faker and map through it inside story component

---
+++ components/Feed.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Stories from './Stories';
+
+export default function Feed() {
+    return (
+        <main>
+            <section>
+                {/* Stories */}
+                <Stories />
+                {/* Post */}
+            </section>
+
+            <section>
+                {/* Mini Profile */}
+                {/* Suggestions */}
+            </section>
+        </main>
+    );
+}
