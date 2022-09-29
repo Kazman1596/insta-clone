b75aca04daeea45b0df849598d9376738e3c483f
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2022-09-28 18:55:51 -0700

    make feed section responsive

--- components/Feed.js
+++ components/Feed.js
@@ -4,15 +4,15 @@ import Stories from './Stories';
 
 export default function Feed() {
     return (
-        <main>
-            <section>
+        <main className='grid grid-cols-1 md:grid-cols-3 mg:max-w-6xl mx-auto'>
+            <section className='md:col-span-2'>
                 {/* Stories */}
                 <Stories />
                 {/* Post */}
                 <Posts />
             </section>
 
-            <section>
+            <section className='hidden md:inline-grid'>
                 {/* Mini Profile */}
                 {/* Suggestions */}
             </section>
