0ea37b2370d7798ecf0bf5fc14746ae8acd4dd7d
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2023-02-24 12:53:57 -0700

    fixed mini-profile showing if signed out

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -5,17 +5,21 @@ export default function MiniProfile() {
     const {data: session} = useSession()
 
     return (
-        <div className="xl:flex items-center justify-between mt-14 ml-10">
-            <div className="flex justify-center">
-                <Image className="h-16 rounded-full border p-[2px]" height='50' width='50' src={session?.user.image} alt="user-image" />
-            </div>
-            <div className="flex-1 ml-4">
-                <h2 className="font-bold">{session?.user.username}</h2>
-                <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
-            </div>
-            <div className="flex justify-center">
-                <button onClick={signOut} className="font-semibold text-blue-800 text-sm">Sign Out</button>
-            </div>
+        <div>
+            {session &&
+                <div className="xl:flex items-center justify-between mt-14 ml-10">
+                    <div className="flex justify-center">
+                        <Image className="h-16 rounded-full border p-[2px]" height='50' width='50' src={session?.user.image} alt="user-image" />
+                    </div>
+                    <div className="flex-1 ml-4">
+                        <h2 className="font-bold">{session?.user.username}</h2>
+                        <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
+                    </div>
+                    <div className="flex justify-center">
+                        <button onClick={signOut} className="font-semibold text-blue-800 text-sm">Sign Out</button>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
